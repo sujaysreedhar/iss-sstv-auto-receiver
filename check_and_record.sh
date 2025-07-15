b905191b867ec40365f8b7be4878cfa1af882509
sujaysreedhar <sujaysreedhar@gmail.com>
2025-07-15 13:16:57 +0530

    fix paths for cron

--- check_and_record.sh
+++ check_and_record.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 SAT="ISS (ZARYA)"
 TLE_FILE="$HOME/iss-sstv-auto-receiver/.predict/predict.tle"
 
 if predict -t "$TLE_FILE" -p "$SAT" | awk '{if ($10 > 10) exit 0; else exit 1}'; then
     echo "Recording ISS SSTV transmission..."
-    ./record_sstv.sh
+    "$SCRIPT_DIR/record_sstv.sh"
     for f in $HOME/iss_sstv/*.wav; do
-        ./decode_sstv.sh "$f"
+        "$SCRIPT_DIR/decode_sstv.sh" "$f"
     done
 else
     echo "No pass at this time."
